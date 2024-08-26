9ce6fcbba736501cfcc7dbb5d05c58ca2278bcbb
Daniel Xerxes <daniel.f.l.r.c@gmail.com>
2024-08-26 15:03:59 -0500

    Taller OCP

--- Taller OCP/EnvioPremium.java
+++ Taller OCP/EnvioPremium.java
@@ -1,6 +1,6 @@
 public class EnvioPremium extends Envio  {
     @Override
     public double calcularCosto(double peso, double distancia) {
-        return peso * 1.5 + distancia * 0.5;
+        return peso * 2 + distancia * 1;
     }
 }
